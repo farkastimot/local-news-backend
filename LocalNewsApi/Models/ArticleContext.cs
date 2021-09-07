1055a8ea687c01cfbaf0ce570849095e0975f18e
Farkas Timot <farkas.timot@gmail.com>
2021-09-07 13:42:24 +0200

    feat: added a function which gets all of the articles

--- LocalNewsApi/Models/ArticleContext.cs
+++ LocalNewsApi/Models/ArticleContext.cs
@@ -19,5 +19,34 @@ namespace LocalNewsApi.Models
         {
             return new MySqlConnection(ConnectionString);
         }
+
+        public List<Article> GetAllArticles()
+        {
+            List<Article> articles = new List<Article>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM articles;", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    articles.Add(new Article()
+                    {
+                        Id = Convert.ToInt32(reader["id"]),
+                        Author = reader["author"].ToString(),
+                        Category = Convert.ToInt32(reader["category"]),
+                        Title = reader["title"].ToString(),
+                        Description = reader["description"].ToString(),
+                        UrlToImage = reader["urlToImage"].ToString(),
+                        PublishedAt = Convert.ToDateTime(reader["publishedAt"]),
+                        Content = reader["content"].ToString()
+                    });
+                }
+            }
+
+            return articles;
+        }
     }
 }
