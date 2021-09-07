520867f85b3c4b4a8eae36015f332de4f273401a
Farkas Timot <farkas.timot@gmail.com>
2021-09-07 13:31:34 +0200

    feat: added ArticleContext with database connection and Article class to store all the data

---
+++ LocalNewsApi/Models/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalNewsApi.Models
+{
+    public class Article
+    {
+        private ArticleContext articleContext;
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public int Category { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string UrlToImage { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ LocalNewsApi/Models/ArticleContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace LocalNewsApi.Models
+{
+    public class ArticleContext
+    {
+        public string ConnectionString { get; set; }
+
+        public ArticleContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
