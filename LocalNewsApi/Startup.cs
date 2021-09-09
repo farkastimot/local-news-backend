63512f72a17b3b736cd07b5f7bce2e319d6eb72e
feherdaniel <feherdaniel2010@gmail.com>
2021-09-09 11:19:18 +0200

    fix: change api accessibility

--- LocalNewsApi/Startup.cs
+++ LocalNewsApi/Startup.cs
@@ -16,6 +16,7 @@ namespace LocalNewsApi
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +27,15 @@ namespace LocalNewsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:3000");
+                                  });
+            });
+
             services.AddControllers();
 
             services.AddSingleton(new DatabaseContext(Configuration.GetConnectionString("DefaultConnection")));
@@ -42,6 +52,8 @@ namespace LocalNewsApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthorization();
 
