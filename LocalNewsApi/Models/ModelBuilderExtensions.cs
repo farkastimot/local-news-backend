01675ceb127050062b738356404a268969e0de9f
feherdaniel <feherdaniel2010@gmail.com>
2021-10-05 09:47:23 +0200

    feat: create ModelBuilderExtensions class to seed database

---
+++ LocalNewsApi/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LocalNewsApi.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                    new Category()
+                    {
+                        Id = 1,
+                        Name = "ModelBuilderTest"
+                    }
+                );
+        }
+    }
+}
