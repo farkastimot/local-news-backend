b7282c69417c71558cfe2a75493bc17b73a73701
feherdaniel <feherdaniel2010@gmail.com>
2021-10-05 09:47:06 +0200

    feat: update database with new migration

--- LocalNewsApi/Migrations/20211005074423_SeedTest.Designer.cs
+++ LocalNewsApi/Migrations/20211005074423_SeedTest.Designer.cs
@@ -4,14 +4,16 @@ using LocalNewsApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LocalNewsApi.Migrations
 {
     [DbContext(typeof(LocalNewsContext))]
-    partial class LocalNewsContextModelSnapshot : ModelSnapshot
+    [Migration("20211005074423_SeedTest")]
+    partial class SeedTest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
