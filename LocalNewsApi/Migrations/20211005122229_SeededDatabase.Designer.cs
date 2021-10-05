90ab5f33c9c973160cbe489dc12df2cf2fcbadc8
feherdaniel <feherdaniel2010@gmail.com>
2021-10-05 14:25:37 +0200

    bugfix: redo migrations because of error, database now seeded fully

--- LocalNewsApi/Migrations/20211005122229_SeededDatabase.Designer.cs
+++ LocalNewsApi/Migrations/20211005122229_SeededDatabase.Designer.cs
@@ -4,14 +4,16 @@ using LocalNewsApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LocalNewsApi.Migrations
 {
     [DbContext(typeof(LocalNewsContext))]
-    partial class LocalNewsContextModelSnapshot : ModelSnapshot
+    [Migration("20211005122229_SeededDatabase")]
+    partial class SeededDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
